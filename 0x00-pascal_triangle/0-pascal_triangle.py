c1520fb902d60f796f49a81c88e3e8902f1e9b66
blvck-root <mtndlovu81@gmail.com>
2024-06-27 22:30:00 +0200

    update code to meet PEP8 coding style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,29 +3,31 @@
 Implement Pascal's Triangle
 """
 
+
 def pascal_triangle(n):
-  """
-  This function generates Pascal's triangle up to a given number of rows (n).
-  It is based on the Binomial Coefficient
+    """
+    This function generates Pascal's triangle up to a given number of rows (n).
+    It is based on the Binomial Coefficient
 
-  Args:
-      n: The number of rows in the Pascal's triangle.
+    Args:
+        n: The number of rows in the Pascal's triangle.
 
-  Returns:
-      A list of lists representing the Pascal's triangle. If n <= 0, returns an empty list.
-  """
-  try:
-    triangle = []
-    if n <= 0:
-      return triangle
+    Returns:
+        A list of lists representing the Pascal's triangle.
+        If n <= 0, returns an empty list.
+    """
+    try:
+        triangle = []
+        if n <= 0:
+            return triangle
 
-    for i in range(1, n + 1):
-      C = 1  # C(i, j)
-      row = []
-      for j in range(1, i + 1):
-        row.append(C)
-        C = int(C * (i - j) / j)
-      triangle.append(row)
-    return triangle
-  except TypeError:
-    print("Input must be an integer")
+        for i in range(1, n + 1):
+            C = 1  # C(i, j)
+            row = []
+            for j in range(1, i + 1):
+                row.append(C)
+                C = int(C * (i - j) / j)
+            triangle.append(row)
+        return triangle
+    except TypeError:
+        print("Input must be an integer")
