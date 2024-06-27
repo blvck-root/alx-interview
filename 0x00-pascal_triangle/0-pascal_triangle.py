5b34dff152ce58cd62a06adb347cdd38ee6d4029
blvck-root <mtndlovu81@gmail.com>
2024-06-27 22:10:15 +0200

    return empty list when n <= 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,6 +15,9 @@ def pascal_triangle(n):
       A list of lists representing the Pascal's triangle. If n <= 0, returns an empty list.
   """
   triangle = []
+  if n <= 0:
+    return triangle
+
   for i in range(1, n + 1):
     C = 1  # C(i, j)
     row = [1]
