03a1f6c71055d5d5b0ba32171c776ff7d931e335
blvck-root <mtndlovu81@gmail.com>
2024-08-22 15:52:22 +0200

    optimize code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,13 +3,24 @@
 
 
 def makeChange(coins, total):
-    """determine the fewest number of coins needed to
-    meet a given amount total.
+    """Determines the fewest number of coins needed to meet a given amount total.
+
+    Args:
+        coins: A list of coin denominations.
+        total: The target amount to achieve.
+
+    Returns:
+        The minimum number of coins required, or -1 if it's not possible.
     """
+
     if total <= 0:
         return 0
-    dp = [0] + [float("inf")] * (total)
+
+    dp = [float("inf")] * (total + 1)
+    dp[0] = 0
+
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
     return dp[-1] if dp[-1] != float("inf") else -1
